0d5b33aef84d95a0de98cd0cf7150f2ed38e5c19
waqar hussain ujjan <wh751212@gmail.com>
2025-06-24 14:37:10 +0500

    a

---
+++ src/calculator/Calculaor.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Calculator = () =>{
+
+return(
+    <>
+
+    <h2>hello</h2>
+    </>
+)
+
+}
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //  import Home from './Home';
-import App from './App';
+// import App from './App';
 // import SignUpPage from './SignUpPage';
+import Calculaor from './calculator/Calculaor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculaor />
+    {/* <App /> */}
     {/* <Home /> */}
     {/* <SignUpPage /> */}
   </React.StrictMode>
